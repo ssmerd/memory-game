6f833775275ff9605eafea7593e245f336cdbf46
ssmerd <>
2023-04-21 16:39:29 +0000

    Add start and reset game button

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -14,6 +14,11 @@ function sortCardsArray(cards) {
 function createCardsGame() {
 
     let cards = document.getElementsByClassName('card');
+    let startGameBtn = document.getElementById('start-game');
+    
+    console.log(startGameBtn);
+
+    startGameBtn.addEventListener('click', startGame);
 
     for (let i = 0; i < cards.length; i++) {
         let image = document.createElement('img');
@@ -21,18 +26,50 @@ function createCardsGame() {
         image.src = 'assets/images/matryoshka-doll-souvenir-toy.png';
         image.alt = 'Colorful Russian Matryoshka';
         cards[i].appendChild(image);
-        image.addEventListener('click', showCard)
     }
+}
+
+function startGame() {
+
+    console.log('start game...')
+
+    let images = document.getElementsByTagName('img');
+
+    for (let i = 0; i < images.length; i++) {
+        console.log(images[i]);
+        images[i].addEventListener('click', showCard)
+    }
+}
+
+var firstCardId = -1;
+var secondCardId = -1;
+
+function hideCard(id) {
+
+    let firstElement = document.querySelector(`[card-id="${id}"]`);
+
+    firstElement.src = 'assets/images/matryoshka-doll-souvenir-toy.png';
+
+    firstElement.classList = "card-hide"; 
 
 }
 
-let firstCardId = -1;
-let secondCardId = -1;
+function hideCards(id1, id2) {
+
+    let firstElement = document.querySelector(`[card-id="${id1}"]`);
+    let secondElement = document.querySelector(`[card-id="${id2}"]`);
+ 
+    firstElement.src = 'assets/images/matryoshka-doll-souvenir-toy.png';
+    secondElement.src = 'assets/images/matryoshka-doll-souvenir-toy.png';
+
+    firstElement.classList = "card-hide"; 
+    secondElement.classList = "card-hide";
+}
 
 function showCard() {
     
     let cardId = this.getAttribute("card-id");
-    
+
     this.src = cards[cardId].src;
     this.alt = cards[cardId].alt;
     this.classList = "card-view";
@@ -45,17 +82,25 @@ function showCard() {
         console.log(secondCardId);
     }
 
+    if (firstCardId === secondCardId) {
+        setTimeout(hideCard, 1000, firstCardId);
+        firstCardId = -1;
+        secondCardId = -1;
+        return;
+    }
+        
     if (secondCardId !== -1) {
         
         let firstElement = document.querySelector(`[card-id="${firstCardId}"]`);
 
         if (firstElement.src == this.src) {
+            firstElement.removeEventListener('click', showCard);
+            this.removeEventListener('click', showCard);
             console.log('yes');
         } else {
-            firstElement.src = 'assets/images/matryoshka-doll-souvenir-toy.png';
-            this.src = 'assets/images/matryoshka-doll-souvenir-toy.png';
-            firstElement.classList = "card-hide"; 
-            this.classList = "card-hide";
+            
+            setTimeout(hideCards, 1000, firstCardId, secondCardId);
+            
             console.log('no');
         }
 
